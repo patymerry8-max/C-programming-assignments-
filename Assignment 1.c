7c91b005db49832da23147edf164650461942871
patymerry8-max <patymerry8@gmail.com>
2025-09-18 14:44:41 +0300

    C programming assignments

---
+++ Assignment 1.c
@@ -0,0 +1,19 @@
+   // units for semester 1
+/*
+Name:Mary Kimilu 
+Reg:26271
+Description:Units for semester 1
+*/
+# include <stdio.h>//pre-processor direct 
+ //main function 
+ int main () {
+ printf("units for semester one;\n");
+ printf("SPH 2107 Analogue Electronics\n");
+ printf("SPI 2102 Computer System and Organisation\n");
+ printf("UPC 2105 Fundamentals of ICT\n");
+ printf("SPI 2103 Introduction to Computer\n");
+ printf("SPI 2120 Essentials of Accounting\n");
+ printf("UCU 2101 Communication skills\n");
+ printf("SPM 2104 Mathematics for science\n");
+ printf("SPI 2101 Introduction to Programming and Algorithims\n");
+ return 0; }
